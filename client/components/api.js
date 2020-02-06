e0f949abce6c2eaeb80f348e1c04fbb3049ab9d4
H-THO <hywel.p.thomas@gmail.com>
2020-02-07 08:13:42 +1300

    added api file

---
+++ client/components/api.js
@@ -0,0 +1,16 @@
+import request from 'superagent'
+
+const catFactsUrl = 'http://localhost:3000/api/v1/catFacts'
+
+export function getFacts () {
+  return request
+    .get(catFactsUrl)
+    .then(response => response.body)
+}
+
+// export function appendWidget (widget) {
+//   return request
+//     .post(widgetUrl)
+//     .send(widget)
+//     .then(response => response.body)
+//   }
